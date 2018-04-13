c9e124f47776baf344c1568fdfd09a4a0e9e64b7
Bullet Zhang <crazybullet28zhang@gmail.com>
2018-04-13 15:33:33 +0200

    matrix functions finish

--- matrix.c
+++ matrix.c
@@ -25,7 +25,6 @@ matrix *initial(int r, int c){
     res->row = r;
     res->column = c;
     res->val = malloc(r*c*sizeof(float));
-//    res->getMat = getMat;
     return res;
 }
 
@@ -34,7 +33,7 @@ void destruct(matrix *self){
     free(self);
 }
 
-struct matrix *addMat(matrix *a, matrix *b){
+matrix *addMat(matrix *a, matrix *b){
     if (a->row != b->row || a->column != b->column){
         fprintf( stderr, "Error in addMat. Matrix size not fit: %d - %d, %d - %d\n", a->row, a->column, b->row, b->column);
         return NULL;
@@ -50,7 +49,7 @@ struct matrix *addMat(matrix *a, matrix *b){
     }
 };
 
-struct matrix *addMatVal(matrix *a, float b){
+matrix *addMatVal(matrix *a, float b){
     matrix *res = initial(a->row, a->column);
     int i=0, j=0;
     for (i=0; i<a->row; i++){
@@ -61,7 +60,7 @@ struct matrix *addMatVal(matrix *a, float b){
     return res;
 };
 
-struct matrix *dotMat(matrix *a, matrix *b){
+matrix *dotMat(matrix *a, matrix *b){
     if (a->column != b->row){
         fprintf( stderr, "Error in dotMat. Matrix size not fit: %d - %d, %d - %d\n", a->row, a->column, b->row, b->column);
         return NULL;
@@ -72,7 +71,7 @@ struct matrix *dotMat(matrix *a, matrix *b){
             for (j=0; j<b->column; j++){
                 *getMat(res, i, j) = 0;
                 for (k=0; k<a->column; k++){
-
+                    *getMat(res, i, j) += *getMat(a, i, k) * *getMat(b, k, j);
                 }
             }
         }
