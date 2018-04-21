0a0625d4b450aca0c9cd50a1d63ccdd387674469
ARCHER <archerwz0805@gmail.com>
2018-04-21 17:36:39 +0200

    correct the error in gradient_update function

--- cnn.c
+++ cnn.c
@@ -402,9 +402,9 @@ void gradient_update(CNN* cnn, CNNOpts opts, matrix* inMat){
     //C1
     for(i=0;i<cnn->C1->outChannels;i++){
         for(j=0;j<cnn->C1->inChannels;j++){
-            matrix* rot_input = initMat(cnn->C1->inputHeight+4,cnn->C1->inputWidth+4,1);
+            matrix* rot_input = initMat(cnn->C1->inputHeight,cnn->C1->inputWidth,1);
             rotate180Mat(rot_input,inMat);
-            covolution_once(cnn->C1->dmapWeight[j][i], rot_input,cnn->C1->d[i], cnn->C1->mapSize, cnn->C1->mapSize, 0);
+            covolution_once(cnn->C1->dmapWeight[j][i], rot_input,cnn->C1->d[i], cnn->C1->mapSize, cnn->C1->mapSize, 2);
             matrix* minus_weight = initMat(cnn->C1->mapSize,cnn->C1->mapSize,1);
             mulMatVal(minus_weight, cnn->C1->dmapWeight[j][i], -1*opts.eta);
             addMat_replace(cnn->C1->mapWeight[j][i],minus_weight);
