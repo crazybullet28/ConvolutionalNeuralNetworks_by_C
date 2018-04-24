43fbd0f02b2386032c7a30ef608314a789582a98
Bullet Zhang <crazybullet28zhang@gmail.com>
2018-04-24 20:43:43 +0200

    no batch. bug not fixed yet

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 int main() {
     srand(100);
-//    const char parentPath[] = "E:/MyDocuments/Documents/CLionProjects/Parallel_Proj/";
 
     LabelArr testLabel=read_Lable("E:/MyDocuments/Documents/CLionProjects/Parallel_Proj/Minst/train-labels.idx1-ubyte");
     ImgArr testImg=read_Img("E:/MyDocuments/Documents/CLionProjects/Parallel_Proj/Minst/train-images.idx3-ubyte");
@@ -21,13 +20,52 @@ int main() {
     trainModel(cnn,testImg,testLabel,opts,trainNum);
 //    testModel()
 
-    FILE  *fp=NULL;
-    fp=fopen("E:/MyDocuments/Documents/CLionProjects/Parallel_Proj/PicTrans/cnnL.ma","wb");
-    if(fp==NULL)
-        printf("write file failed\n");
-    fwrite(cnn->L,sizeof(float),trainNum,fp);
-    fclose(fp);
+//    FILE  *fp=NULL;
+//    fp=fopen("E:/MyDocuments/Documents/CLionProjects/Parallel_Proj/PicTrans/cnnL.ma","wb");
+//    if(fp==NULL)
+//        printf("write file failed\n");
+//    fwrite(cnn->L,sizeof(float),trainNum,fp);
+//    fclose(fp);
 
+//    matrix *v, *inMat, *map;
+//
+//    v = initMat(5, 5, 1);
+//    inMat = initMat(5, 5, 0);
+//    map = initMat(5, 5, 0);
+//
+//    int i, j;
+//
+//    for (i=0; i<25; i++){
+//        inMat->val[i] = i;
+//        map->val[i] = 1;
+//    }
+//
+//    for (i=0; i<5; i++){
+//        for (j=0; j<5; j++){
+//            printf("%.4f ", *getMatVal(inMat, i, j));
+//        }
+//        printf("\n");
+//    }
+//
+//    printf("\n");
+//
+//    for (i=0; i<5; i++){
+//        for (j=0; j<5; j++){
+//            printf("%.4f ", *getMatVal(map, i, j));
+//        }
+//        printf("\n");
+//    }
+//
+//    printf("\n");
+//
+//    covolution_once(v, inMat, map, 5, 5, 2);
+//
+//    for (i=0; i<5; i++){
+//        for (j=0; j<5; j++){
+//            printf("%.4f ", *getMatVal(v, i, j));
+//        }
+//        printf("\n");
+//    }
 
     return 0;
 }
